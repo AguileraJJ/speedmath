1f96358b2956f1827ef2a7354d38e223c24f5a07
Juan Aguilera <aguilera.juanjuan@gmail.com>
2025-05-18 09:52:09 -0600

    route added

--- src/Controller/Numbers.php
+++ src/Controller/Numbers.php
@@ -6,22 +6,32 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class Numbers extends AbstractController
-{
-    #[Route('/')]
-    public function number(): Response
-    {
-        
-        $number1 = random_int(0, 12);
-        $number2 = random_int(0, 12);
+class Numbers extends AbstractController {
 
+    #[Route('/')]
+    public function index(): Response {
+
+        return $this->render('base.html.twig', []);
+    }
+
+    #[Route('/multiplier', name: 'multiplier')]
+    public function number(): Response {
+        $selected = 8;
+        
+        $number = random_int(0, $selected);
+        
         return $this->render('numbers/number.html.twig', [
-            'number1' => $number1,
-            'number2' => $number2,
+            'number' => $number,
         ]);
     }
 
-    public function multiplication($num1,$num2) {
-        return $num1*$num2;
+    #[Route('/lucky/number')]
+    public function numbers(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
     }
 }
