80800bc00f7842e21df0c186137db4435aae14b1
idlottery-jaguilera <163052015+idlottery-jaguilera@users.noreply.github.com>
2025-06-13 14:34:24 -0600

    response with page

--- src/Controller/Numbers.php
+++ src/Controller/Numbers.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,12 +19,29 @@ class Numbers extends AbstractController {
     #[Route('/multiplier/{selected}', name: 'app_multiplier')]
     public function number(int $selected): Response {
         
-        $number = random_int(0, $selected);
+        $number1 = random_int($selected, 12);
+        $number2 = random_int($selected, 12);
         
 
         return $this->render('numbers/number.html.twig', [
-            'number' => $number,
+            'number1' => $number1,
+            'number2' => $number2,
         ]);
+    }
+
+    #[Route('/check-answer', name:'check_answer')]
+    public function checkAnswer(Request $request) : Response {
+        $userAnswer = $request->request->get('answer');
+        $number1 = $request->request->get('number1');
+        $number2 = $request->request->get('number2');
+
+        $correctAnswer = $number1 * $number2;
+
+        if ($userAnswer == $correctAnswer) {
+            return new Response('Correct!');
+        }
+
+        return new Response('Wrong answer, try again!');
     }
 
 }
